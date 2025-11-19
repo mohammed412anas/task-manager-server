fda8c84e4219dbac8c6974bdfa135fd49b26a187
mohammed412anas <mohammed.anas@everest.engineering>
2025-11-20 03:32:23 +0530

    Update server to host the backend application

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-import express, { Express, Request ,Response} from "express";
+import express, { Express } from "express";
 import cors, { CorsOptions } from "cors";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import router from "./routes/router";
 
-dotenv.config()
+dotenv.config();
 const app: Express = express();
-const PORT = process.env.PORT || 10000 ;
+const PORT = process.env.PORT || 10000;
 const corseOptions: CorsOptions = {
   origin: true,
   methods: ["GET", "PUT", "POST", "DELETE"],
@@ -12,8 +13,7 @@ const corseOptions: CorsOptions = {
 
 app.use(cors(corseOptions));
 app.use(express.json());
-
-app.get('/',(req:Request,res:Response)=>res.send('Hello World!'))
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
